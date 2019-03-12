26b20808937ffebd0053580fb47083b2a19e5fc8
EconClass <bizmail327@gmail.com>
2019-03-11 20:22:26 -0700

    basic setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if (!process.env.PORT) {
+  require('dotenv').config()
+  process.env.NODE_ENV = "dev"
+}
+
+const express = require('express'),
+      bodyparser = require('body-parser'),
+      methodoverride = require('method-override'),
+      app = express(),
+      port = process.env.PORT || 3000;
+
+//========================MIDDLEWARE========================\\
+
+// Dependencies
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(methodoverride);
+
+// Mongoose Connection
+require('./data/poketeam-db');
+
+// Routers
+
+// Access Port
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`)
+})

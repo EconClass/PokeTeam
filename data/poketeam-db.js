3e0ec7a20708686e6ef41a84b56c3a8b9d30ec6c
EconClass <bizmail327@gmail.com>
2019-03-11 20:22:06 -0700

    basic setup

---
+++ data/poketeam-db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose'),
+      assert = require('assert');
+
+const url = process.env.MONGODB_URI || "mongodb://localhost/poketeam-db";
+
+mongoose.connect(url, { useNewUrlParser: true },
+  function(err, _){
+    assert.equal(null, err)
+    console.log('DB connection successful.')
+  }
+);
+
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;

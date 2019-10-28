0c837d300f122fc40c886f33ab143c86e7eef2f8
EconClass <bizmail327@gmail.com>
2019-10-28 15:22:20 -0700

    [mod] useUnifiedTopology for mongoose

--- data/poketeam-db.js
+++ data/poketeam-db.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose'),
-assert = require('assert');
+  assert = require('assert');
 
 const url = process.env.MONGODB_URI || "mongodb://localhost/poketeam-db"; // || "mongodb://mongo/poketeam-db";
 
-mongoose.connect(url, { useNewUrlParser: true },
-  function(err, _){
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},
+  function (err, _) {
     assert.equal(null, err)
     console.log('DB connection successful.')
   },
-  { useFindAndModify: false }
+  { useFindAndModify: false },
 );
 
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
-mongoose.set("debug", true);
+mongoose.set("debug", false);
 
 module.exports = mongoose.connection;
